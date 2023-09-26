24290e9ff2b57be85f7a3f99ed4aac61d636ab74
nullnummer24 <j.rohrbach@sensemail.ch>
2023-09-26 16:26:34 +0200

    save

--- main.go
+++ main.go
@@ -26,9 +26,44 @@ func main() {
 		return
 	}
 
+	// Ask the user what he wants to do
+	// c := exec.Command("clear")
+	// fmt.Printf("Select what you want to do:")
+	// for {
+	// 	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+	// 		if key.Code == keys.ArrowDown {
+	// 			c.Stdout = os.Stdout
+	// 			c.Run()
+
+	// 		}
+	// 		return false, nil
+	// 	})
+	// 	keyInfo, _ := keyboard.GetKey()
+	// 	key := keyInfo.Code
+
+	// 	if key == keys.ArrowDown {
+	// 		c.Stdout = os.Stdout
+	// 		c.Run()
+
+	// 	}
+	// 	fmt.Printf("test")
+	// 	keyboard.StopListener()
+
+	// 	if key == keys.ENTER {
+	// 		break
+	// 	}
+	// }
+
 	// Print the fetched data
 	for _, pokemon := range pokemons {
-		fmt.Printf("ID: %d\nName: %s\nHP: %s\nType: %s\n %s \n", pokemon.ID, pokemon.Name, pokemon.HP, pokemon.TYPE, pokemon.ASCII)
+		var elements string
+		for _, type0 := range pokemon.TYPE {
+			elements += type0.TYPE + ", "
+
+		}
+
+		elements = elements[:len(elements)-1]
+		fmt.Printf("ID: %d\nName: %s\nHP: %s\nType: %s\n %s \n", pokemon.ID, pokemon.Name, pokemon.HP, elements, pokemon.ASCII)
 	}
 }
 
